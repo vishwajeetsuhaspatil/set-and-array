89d40afe3a97ce5126dc429b343c8d8e19f33e36
vishwajeetsuhaspatil <bhagyashreesushv2@gmail.com>
2024-09-15 20:37:30 +0530

    set and array

---
+++ set.py
@@ -0,0 +1,15 @@
+set= {1,2,3,3}
+print(set)
+a= {1.0,"Hello"}
+print(a)
+print(a.pop())
+x= {"red","cyan"}
+y= {"red","green"}
+z= x.union(y)
+print(z)
+b= x.intersection(y)
+c= x.symmetric_difference(y)
+d= x.difference(y)
+print(b)
+print(c)
+print(d)
